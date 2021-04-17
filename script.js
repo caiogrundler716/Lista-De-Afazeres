81112a2e24d47dd490065afc2845f8c934811d99
Caio Grundler <caiob.grundler2002@gmail.com>
2021-04-17 18:10:18 -0300

    arrumando o css

--- script.js
+++ script.js
@@ -1,39 +1,88 @@
-const addButton = document.querySelector('.addButton');
+const container = document.querySelector('.container');
+var inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
 
-var inputValue = document.querySelector('.input')
+if(window.localStorage.getItem("todos") == undefined){
+     var todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
+}
 
-const container = document.querySelector('.container')
+var todosEX = window.localStorage.getItem("todos");
+var todos = JSON.parse(todosEX);
+
 
 class item{
-    constructor(itemName){
-        this.createDiv(itemName)
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	var itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	var input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
+
+    	var edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = "EDIT";
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	var remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "REMOVE";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
 
     }
 
-createDiv(itemName){
-    let input = document.createElement('input');
-    input.value = itemName;
-    input.disabled = true;
-    input.classList.add('item_input');
-    input.type = "text";
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
 
-    let itemBox = document.createElement('div');
-    itemBox.classList.add('item');
-
-    let editButton = document.createElement('button');
-    editButton.classList.add('editButton');
-
-    let removeButton = document.createElement('button');
-    removeButton.classList.add('removeButton')
-
-    container.appendChild(itemBox);
-
-    itemBox.appendChild(input);
-    itemBox.appendChild(editButton);
-    itemBox.appendChild(removeButton);
-
- }
-
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
 }
 
-new item("Atividade Fisica")
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
+
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
+}
+
+
+for (var v = 0 ; v < todos.length ; v++){
+    new item(todos[v]);
+}
+
+
+new item("Esporte");
