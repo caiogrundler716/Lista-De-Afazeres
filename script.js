0e99d506d4c7de121efba73a285c626ad19381f7
Caio Grundler <caiob.grundler2002@gmail.com>
2021-04-17 12:37:22 -0300

    querySelector do script

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 const addButton = document.querySelector('.addButton');
+
 var inputValue = document.querySelector('.input')
+
+const container = document.querySelector('.container')
+
+class item{
+    constructor(itemName){
+        this.createDiv(itemName)
+
+    }
+
+createDiv(itemName){
+    let input = document.createElement('input');
+    input.value = itemName;
+    input.disabled = true;
+    input.classList.add('item_input');
+    input.type = "text";
+
+    
+
+    let editButton = document.createElement('button');
+    editButton.classList.add('editButton');
+
+    let removeButton = document.createElement('button');
+    removeButton.classList.add('removeButton')
+
+
+}
+
+}
